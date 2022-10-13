6065eb063d9515ed24235227c87a3a06d61bb0f3
anniesyj <anniesong.yj@gmail.com>
2022-10-14 00:34:09 +0900

    developing signUp page

--- src/components/Auth.vue
+++ src/components/Auth.vue
@@ -1,13 +1,23 @@
 <template>
 <v-app>
     <v-container fluid class="fill-height teal">
-        <v-row>
             <v-col cols="12" xs="12" sm="8" md="8" lg="7" class="ma-auto">
-                <v-card outlined class="pa-10">
+                <v-card outlined class="pa-10 mt-10">
                     <v-card-title class="justify-center" style="font-size: 2rem">회원가입</v-card-title>
                     <v-spacer></v-spacer>
                     <v-card-text>
                         <form @submit.prevent="signUp">
+							<div class="mb-5">
+								<picture-input
+									ref="pictureInput"
+									width="150" height="160" margin="5" accept="image/*" size="10" button-class="btn"
+									:custom-strings="{
+										upload: '<h1>Upload Here!</h1>',
+										drag: '프로필 사진 등록'
+									}"
+									@change="onChange">
+								</picture-input>
+							</div>
                             <v-text-field
                                 clearable
                                 dense
@@ -46,57 +56,58 @@
                                 type="password"
                                 v-model="passwordChk">
                             </v-text-field>
+							<v-select
+								v-model="gender"
+								:items="genderSel"
+								dense
+								label="성별"
+								outlined>
+							</v-select>
+							<v-menu
+								ref="menu"
+								v-model="menu"
+								:close-on-content-click="false"
+								:nudge-right="40"
+								transition="scale-transition"
+								offset-y
+								min-width="auto">
+								<template v-slot:activator="{ on, attrs }">
+								<v-text-field
+									v-model="birthdate"
+									label="생년월일"
+									prepend-icon="mdi-calendar"
+									readonly
+									v-bind="attrs"
+									v-on="on"
+								></v-text-field>
+								</template>
+								<v-date-picker
+								v-model="birthdate"
+								@input="menu=false"
+								>
+								<v-spacer></v-spacer>
+								<v-btn
+									text
+									color="primary"
+									@click="menu = false"
+								>
+									Cancel
+								</v-btn>
+								<v-btn
+									text
+									color="primary"
+									@click="$refs.menu.save(birthdate)"
+								>
+									OK
+								</v-btn>
+								</v-date-picker>
+							</v-menu>
 
-                            <v-row align="center">
-                                <v-col class="d-flex" cols="12" sm="6">
-                                    <v-text-field
-                                        clearable
-                                        dense
-                                        outlined
-                                        label="나이"
-                                        type="number"
-                                        v-model.number="age">
-                                    </v-text-field>
-                                </v-col>
-                                <v-col class="d-flex" cols="12" sm="6">
-                                        <v-select
-                                            v-model="gender"
-                                            :items="genderSel"
-                                            dense
-                                            label="성별"
-                                            outlined>
-                                        </v-select>
-                                </v-col>
-                            </v-row>
-
-                            <v-divider class="pa-5"></v-divider>
-                            
-                            <v-row>
-                                <v-col class="d-flex" cols="2" sm="6">
-                                    <div>
-                                    <subheader>프로필 사진</subheader>
-                                        <picture-input
-                                            ref="pictureInput"
-                                            width="150" height="160" margin="5" accept="image/*" size="10" button-class="btn"
-                                            :custom-strings="{
-                                                upload: '<h1>Upload Here!</h1>',
-                                                drag: 'Upload your profile picture here'
-                                            }"
-                                            @change="onChange">
-                                        </picture-input>
-                                    </div>
-                                </v-col>
-                                <v-col class="d-flex" cols="12" sm="6">
-                                    <div>
-                                        <subheader>바이오</subheader>
-                                        <v-textarea filled background-color="#FAEBD7"
-                                        counter="150"
-                                            v-model="profileMessage"  placeholder="Write your profile message here">
-                                          </v-textarea>
-                                    </div>
-                                </v-col>
-                            </v-row>
-
+							<v-subheader>상태 메세지</v-subheader>
+							<v-textarea filled background-color="#FAEBD7"
+							counter="150"
+							v-model="profileMessage"  placeholder="원하는 상태 메시지를 작성하세요!">
+							</v-textarea>
                             <v-divider class="ma-4"></v-divider>
 
                             <div>
@@ -108,7 +119,6 @@
                     </v-card-text>
                 </v-card>
             </v-col>
-        </v-row>
     </v-container>
 </v-app>
 </template>
@@ -116,7 +126,7 @@
 <script>
 import axios from 'axios';
 import PictureInput from 'vue-picture-input'
-// import { mapActions } from 'vuex'
+import { mapActions } from 'vuex'
 
 export default {
     data () {
@@ -126,34 +136,33 @@ export default {
             userId: '',
             password: '',
             passwordChk: '',
-            age: '',
+            birthdate: '',
             gender: '',
             photoUrl: 'no-img.jpg',
             profileMessage: '',
             genderSel: ['여', '남'],
             loading: false,
+			picker: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0,10),
+			menu: false,
+			rPath: '/Post',
         }
-    },
+	},
     computed: {
         invalidForm() {
             return !this.userId || !this.password
         }
     },
+	created() {
+    this.rPath = this.$route.query.rPath || '/'
+  },
     components: {
-        PictureInput
+        PictureInput,
     },
     methods: {
-        onChange (image) {
-            console.log("new picture selected")
-            if(image) {
-                console.log('picture loaded')
-                this.photoUrl = image
-            } else {
-                console.log('FileReader API not supported.')
-            }
-        },
-
-        async signUp() {
+		...mapActions([
+			'SIGNUP'
+		]),
+        signUp() {
             if (this.loading) return;
             this.loading = true;
 
@@ -162,15 +171,31 @@ export default {
                 email: this.email,
                 userId: this.userId,
                 password: this.password,
-                age: this.age,
+                birthdate: this.birthdate,
                 gender: this.gender,
                 photoUrl: this.photoUrl,
                 profileMessage: this.profileMessage,
             };
-            console.log('auth/signUp - axiosBody: ', axiosBody);
-        } 
+			console.log(axiosBody);
+				this.SIGNUP( axiosBody )
+			.then(
+				this.$router.push(this.rPath)
+			)
+			.catch(err=> {
+				console.log(err);
+			})
+        },
+		onChange (image) {
+            console.log("new picture selected")
+            if(image) {
+                console.log('picture loaded')
+                this.photoUrl = image
+            } else {
+                console.log('FileReader API not supported.')
+            }
+        },
     }
-    }
+}
 </script>
 
 <style>
@@ -180,4 +205,7 @@ export default {
   margin: 10px 26px 0 0;
 }
 
+.row {
+	justify-content: space-evenly;
+}
 </style>
